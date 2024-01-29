bde2c79e0b2322d84e5196cfb2bb0ea9374f67a2
omarbdw <omarbdw@yahoo.com>
2024-01-29 13:42:10 +0200

    betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,18 @@
 #include "binary_trees.h"
-void binary_tree_delete(binary_tree_t *tree) {
-	if (tree == NULL) {
-		return;
-	}
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+if (tree == NULL)
+return;
 
-	// Delete the left subtree
-	binary_tree_delete(tree->left);
+binary_tree_delete(tree->left);
 
-	// Delete the right subtree
-	binary_tree_delete(tree->right);
+binary_tree_delete(tree->right);
 
-	// Delete the current node
-	free(tree);
+free(tree);
 }
