01829b6894fba0bdc697ab68225e0b99df9b8b91
omarbdw <omarbdw@yahoo.com>
2024-01-31 10:47:00 +0200

    4

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,11 +8,11 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 if (parent == NULL)
-return (NULL);
+return NULL;
 
 binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
-return (NULL);
+return NULL;
 
 new_node->n = value;
 new_node->parent = parent;
@@ -24,5 +24,5 @@ parent->right->parent = new_node;
 
 parent->right = new_node;
 
-return (new_node);
+return new_node;
 }
