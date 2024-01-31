8993e95efaebc6eb0de032d093401e04dbd126f5
omarbdw <omarbdw@yahoo.com>
2024-01-31 11:01:30 +0200

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,12 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 int left_height = 0, right_height = 0;
+
 if (tree == NULL)
 return (0);
-if (tree->left)
+if (tree->left != NULL)
 left_height = 1 + binary_tree_height(tree->left);
-if (tree->right)
+if (tree->right != NULL)
 right_height = 1 + binary_tree_height(tree->right);
 return (left_height - right_height);
 }
