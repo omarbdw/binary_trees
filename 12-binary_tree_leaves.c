5eb729a2736334940c3ae67b28fd775ebf6ec8ed
omarbdw <omarbdw@yahoo.com>
2024-01-31 10:59:06 +0200

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,14 +7,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+size_t leaves = 0;
 if (tree == NULL)
 return (0);
-
 if (tree->left == NULL && tree->right == NULL)
 return (1);
-
-size_t left_leaves = binary_tree_leaves(tree->left);
-size_t right_leaves = binary_tree_leaves(tree->right);
-
-return (left_leaves + right_leaves);
+leaves += binary_tree_leaves(tree->left);
+leaves += binary_tree_leaves(tree->right);
+return (leaves);
 }
