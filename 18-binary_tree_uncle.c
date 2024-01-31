ee1d22a0bd9de3f62a96aae2b7f02112008bf7ef
omarbdw <omarbdw@yahoo.com>
2024-01-31 11:07:21 +0200

    18b

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 binary_tree_t *parent;
 
 if (node == NULL || node->parent == NULL ||
-    node->parent->parent == NULL)
+node->parent->parent == NULL)
 return (NULL);
 
 parent = node->parent;
