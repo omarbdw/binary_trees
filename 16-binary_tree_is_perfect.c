9f5cebd1096c6f9219c3194a2a9f31ee43a5d82e
omarbdw <omarbdw@yahoo.com>
2024-01-31 11:02:34 +0200

    15

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,9 +12,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
-
 if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree))
 return (1);
-
 return (0);
 }
