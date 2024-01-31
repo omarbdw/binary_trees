491a1287fcab338043c5d3b5324b97e1e09f5c37
omarbdw <omarbdw@yahoo.com>
2024-01-31 11:00:35 +0200

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,16 +7,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+size_t nodes = 0;
 if (tree == NULL)
 return (0);
-
-size_t count = 0;
-
 if (tree->left != NULL || tree->right != NULL)
-count++;
-
-count += binary_tree_nodes(tree->left);
-count += binary_tree_nodes(tree->right);
-
-return (count);
+nodes++;
+nodes += binary_tree_nodes(tree->left);
+nodes += binary_tree_nodes(tree->right);
+return (nodes);
 }
