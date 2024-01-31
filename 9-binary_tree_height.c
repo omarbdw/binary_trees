b932bfa6d0532003dd125d2518fea57d01a152e7
omarbdw <omarbdw@yahoo.com>
2024-01-31 10:57:43 +0200

    7

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,17 +7,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height = 0;
-size_t right_height = 0;
-
+size_t left_height = 0, right_height = 0;
 if (tree == NULL)
 return (0);
-
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-
+if (tree->left != NULL)
+left_height = 1 + binary_tree_height(tree->left);
+if (tree->right != NULL)
+right_height = 1 + binary_tree_height(tree->right);
 if (left_height > right_height)
-return (left_height + 1);
+return (left_height);
 else
-return (right_height + 1);
+return (right_height);
 }
